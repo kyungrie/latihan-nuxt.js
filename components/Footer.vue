4e0d407bf8c50a30f453502d73eec90619cee268
ARIE\Lenovo <kashikiku.kyungrie12@gmail.com>
2023-10-17 07:49:31 +0700

    update

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,5 +1,9 @@
 <template>
-    <footer>
-        <h1>SMK CODING</h1>
+    <footer class="w-full border-b border-slate-200 py-6">
+        <div class="container">
+            <div class="flex justify-between items-center">
+                <p>&copy; 2023. SMKcoding. All rights reserved</p>
+            </div>
+        </div>
     </footer>
 </template>
